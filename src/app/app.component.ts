93e21d739b91907b622b57eff2cdb0e27f09e0f1
Fleesin <jean.quintana.123@gmail.com>
2025-04-06 01:40:09 -0500

    add Bootstrap Icons and enhance app layout with new styles and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
