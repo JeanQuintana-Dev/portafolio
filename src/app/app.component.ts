a39dc140c6547f2b1680b745f8956ca3f0d93ed9
Fleesin <jean.quintana.123@gmail.com>
2025-04-12 00:13:03 -0500

    Add footer and WhatsApp components with styles and tests
    - Created FooterComponent with HTML and CSS for footer layout.
    - Implemented WhatsAppComponent with a floating button linking to WhatsApp.
    - Added styles for both components to enhance UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { WhatsappComponent } from './whatsapp/whatsapp.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, WhatsappComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
