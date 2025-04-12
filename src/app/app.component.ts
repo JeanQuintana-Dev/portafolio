308f425b020288331e2bbc48c5c5b7a7c59a2c43
Fleesin <jean.quintana.123@gmail.com>
2025-04-11 22:20:25 -0500

    Add Home component with template, styles, and tests; remove unused code from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
-
+import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
